7d2cd7409da02410786d93d067623b9a614cd497
hiijitesh <jitesbharti@gmail.com>
2022-10-13 15:05:36 +0530

    practice started

---
+++ start_pattern/increasing_trinagle.py
@@ -0,0 +1,9 @@
+
+# n = int(input())
+n = 7
+
+for row in range(n):
+    for col in range(row+1):
+        print("* ", end = "")
+
+    print()
---
+++ start_pattern/right_angle_triangle_2.py
@@ -0,0 +1,9 @@
+
+n = 5
+
+for row in range(n):
+    for col in range (1, row):
+    print("", end= "")
+
+    for col in
+    print("*")
